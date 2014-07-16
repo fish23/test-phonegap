788704deaaf6bb18e712284e5bffc726d30dbf25
bomton <bomton@mint.bomton.cz>
2014-07-16 13:25:07 +0200

    debugging take a photo, removed ajax.post after take

--- js/photo.js
+++ js/photo.js
@@ -3,17 +3,18 @@ var photo = {
     {
         navigator.camera.getPicture(photo.success, photo.error, {
             quality: 85, 
-            destinationType: Camera.DestinationType.FILE_URI,
+            destinationType: Camera.DestinationType.DATA_URL,
             encodingType: Camera.EncodingType.JPEG,
+            mediaType: Camera.MediaType.PICTURE,
+            allowEdit : true,
             sourceType : Camera.PictureSourceType.CAMERA
         });
     },
     success: function(imageData)
     {
-        ajax.post(imageData);
         navigator.notification.alert("Photo has been successfully retrived!", app.blankEvent);
         var imgPhoto = document.getElementById('img-photo');
-        imgPhoto.src = imageData;
+        imgPhoto.src = 'data:image/jpeg;base64,' + imageData;
         imgPhoto.style.display = 'block';
 
     },
