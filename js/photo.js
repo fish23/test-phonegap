0b3ace9dcca5881c4cd69d617d5f2c7c96b25ede
bomton <bomton@mint.bomton.cz>
2014-07-16 14:07:40 +0200

    debugging take a photo, set back destination to DATA_URL and set tagetWidth and Height

--- js/photo.js
+++ js/photo.js
@@ -1,34 +1,41 @@
 var photo = {
+    width: 320, //1024,
+    height: 200, //768,
     get: function()
     {
-        navigator.camera.getPicture(photo.success, photo.error, {
-            quality: 85, 
-            destinationType: Camera.DestinationType.FILE_URI,
+        var options = {
+            quality: 70, 
+            destinationType: Camera.DestinationType.DATA_URL,
             encodingType: Camera.EncodingType.JPEG,
             mediaType: Camera.MediaType.PICTURE,
-            allowEdit : true,
             sourceType : Camera.PictureSourceType.CAMERA
-        });
+        };
+
+        if(window.innerWidth > window.innerHeight)
+        {
+            options.tagetWidth = photo.width;
+            options.tagetHeight = photo.height;
+        }
+        else
+        {
+            options.tagetWidth = photo.height;
+            options.tagetHeight = photo.width;
+        }
+
+        navigator.camera.getPicture(photo.success, photo.error, options);
     },
     success: function(imageData)
     {
         navigator.notification.alert("Photo has been successfully retrived!", app.blankEvent);
-        window.resolveLocalFileSystemURL(imageData, photo.place, photo.error);
-
+        var imgPhoto = document.getElementById('img-photo');
+        imgPhoto.src = 'data:image/jpeg;base64,' + imageData;
+        imgPhoto.style.display = 'block';
     },
     error: function()
     {
-        navigator.notification.alert("Unable to get Photo!", app.blankEvent);
         var imgPhoto = document.getElementById('img-photo');
         imgPhoto.src = '';
         imgPhoto.style.display = 'none';
-    },
-    place: function(fileEntry)
-    {
-        var imgPhoto = document.getElementById('img-photo');
-        console.log('fish', fileEntry.fullPath);
-        imgPhoto.src = fileEntry.fullPath;
-        imgPhoto.style.display = 'block';
-        
+        navigator.notification.alert("Unable to get Photo!", app.blankEvent);
     }
 }
