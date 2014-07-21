7e7738bbe134f614bad6cd68b4794505b5115bc3
bomton <bomton@mint.bomton.cz>
2014-07-21 16:56:56 +0200

    debug added facebook js file local

--- js/index.js
+++ js/index.js
@@ -9,13 +9,13 @@ if ( XMLHttpRequest.prototype.sendAsBinary === undefined ) {
     };
 };
 
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "http://connect.facebook.net/en_US/all.js";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
+//(function(d, s, id) {
+//  var js, fjs = d.getElementsByTagName(s)[0];
+//  if (d.getElementById(id)) return;
+//  js = d.createElement(s); js.id = id;
+//  js.src = "http://connect.facebook.net/en_US/all.js";
+//  fjs.parentNode.insertBefore(js, fjs);
+//}(document, 'script', 'facebook-jssdk'));
 
 window.fbAsyncInit = function() {
     FB.init({
