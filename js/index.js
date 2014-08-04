16be2bed9c3d20b949fcb049e10abf5fda1bcf3f
bomton <bomton@mint.bomton.cz>
2014-08-04 17:07:05 +0200

    completly rebuilded, old files stink, on ionic

--- js/index.js
+++ js/index.js
@@ -1,32 +1,3 @@
-/* Facebook Canvas share initialization */
-// from: http://stackoverflow.com/a/5303242/945521
-if ( XMLHttpRequest.prototype.sendAsBinary === undefined ) {
-    XMLHttpRequest.prototype.sendAsBinary = function(string) {
-        var bytes = Array.prototype.map.call(string, function(c) {
-            return c.charCodeAt(0) & 0xff;
-        });
-        this.send(new Uint8Array(bytes).buffer);
-    };
-};
-
-//(function(d, s, id) {
-//  var js, fjs = d.getElementsByTagName(s)[0];
-//  if (d.getElementById(id)) return;
-//  js = d.createElement(s); js.id = id;
-//  js.src = "http://connect.facebook.net/en_US/all.js";
-//  fjs.parentNode.insertBefore(js, fjs);
-//}(document, 'script', 'facebook-jssdk'));
-
-window.fbAsyncInit = function() {
-    FB.init({
-      appId : "813637635336149", //mobileTesting
-      status : true,
-      cookie : true,
-      xfbml : true // parse XFBML
-    });
-};
-/* Facebook Canvas share initialization END */
-
 var app = {
     isReady: false,
     // Application Constructor
@@ -49,21 +20,10 @@ var app = {
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
         app.isReady = true;
-        navigator.notification.alert("Hello - Device is Ready!", app.blankEvent);
-        console.log('Received Event: ' + id);
-        
 
         if (window.device.platform === "iOS" && parseFloat(window.device.version) >= 7.0) {
-          var appElement = document.getElementById('app');
-          appElement.style.top = "20px";
-          appElement.style.background = "#123123";
+            // ...
         }
     },
     blankEvent: function() {
